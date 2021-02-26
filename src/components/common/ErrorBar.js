6554ff51831ef4d6b92c6875f83fa32d8a8c1bc0
Bhupalam, Naga Pradeep (US - Hyderabad) <nbhupalam@deloitte.com>
2021-02-26 15:29:24 +0530

    updated error component

--- src/components/common/ErrorBar.js
+++ src/components/common/ErrorBar.js
@@ -1,21 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { Snackbar } from "@material-ui/core";
 
 import { getErrors } from "../../store/actions/errorActions";
 
-class ErrorBar extends Component {
-  handleSnackClose = () => {
-    this.props.getErrors("");
-  };
-  render() {
-    const { isErrorbarOpen, errorMessage } = this.props;
-    return (
-      <Snackbar open={isErrorbarOpen} message={<span>{errorMessage}</span>} />
-    );
-  }
-}
+const ErrorBar = ({ isErrorbarOpen, errorMessage }) => {
+  return (
+    <Snackbar open={isErrorbarOpen} message={<span>{errorMessage}</span>} />
+  );
+};
 
 ErrorBar.propTypes = {
   isErrorbarOpen: PropTypes.bool.isRequired,
